e49c8fdc8ecbc2924f5f7c9fc1864dce87813315
Naval Kishore <usnavalg@gmail.com>
2026-02-01 12:25:12 +0530

    Time: 1 ms (72.94%), Space: 56 MB (50.63%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    if (x < 2) return x;
+
+    let left = 1;
+    let right = Math.floor(x / 2);
+    let answer = 0;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        const sq = mid * mid;
+
+        if (sq === x) {
+            return mid;
+        } else if (sq < x) {
+            answer = mid;
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return answer;
+};
