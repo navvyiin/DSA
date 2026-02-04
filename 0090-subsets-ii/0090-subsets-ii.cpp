b95d41609e8e866d999bfd4381638b52d71c74c5
Naval Kishore <usnavalg@gmail.com>
2026-02-04 22:52:08 +0530

    Time: 0 ms (100%), Space: 10.3 MB (96.64%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+    void backtrack(int index, vector<int>& nums,
+                   vector<int>& current,
+                   vector<vector<int>>& result) {
+
+        result.push_back(current);
+
+        for (int i = index; i < nums.size(); i++) {
+            // Skip duplicates
+            if (i > index && nums[i] == nums[i - 1])
+                continue;
+
+            current.push_back(nums[i]);
+            backtrack(i + 1, nums, current, result);
+            current.pop_back();
+        }
+    }
+
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> result;
+        vector<int> current;
+
+        backtrack(0, nums, current, result);
+        return result;
+    }
+};
