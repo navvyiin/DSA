7738d18c7272f1e690b25b5a5c1ca692f66815f3
Naval Kishore <usnavalg@gmail.com>
2026-02-21 13:40:05 +0530

    Time: 2 ms (94.52%), Space: 27.6 MB (95.08%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<int> dp(n + 1, 0);
+        int maxSide = 0;
+        int prev = 0;  // dp[j-1] from previous row (top-left)
+
+        for (int i = 1; i <= m; i++) {
+            prev = 0;
+            for (int j = 1; j <= n; j++) {
+                int temp = dp[j];
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[j] = min({dp[j], dp[j - 1], prev}) + 1;
+                    maxSide = max(maxSide, dp[j]);
+                } else {
+                    dp[j] = 0;
+                }
+                prev = temp;
+            }
+        }
+
+        return maxSide * maxSide;
+    }
+};
