8569859aa22423b7758002e5722a4ee2fc450592
Naval Kishore <usnavalg@gmail.com>
2026-02-23 18:16:05 +0530

    Time: 0 ms (100%), Space: 9.8 MB (47.63%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int freq[26] = {0};
+
+        for (char c : s) {
+            freq[c - 'a']++;
+        }
+
+        for (char c : t) {
+            freq[c - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
