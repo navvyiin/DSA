fa0e74b5bc6b2b90a824a1373d1758e3d23e3280
Naval Kishore <usnavalg@gmail.com>
2026-02-23 18:24:27 +0530

    Time: 1 ms (70.39%), Space: 56.9 MB (48.35%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,10 @@
+var missingNumber = function(nums) {
+    let xor = nums.length;
+
+    for (let i = 0; i < nums.length; i++) {
+        xor ^= i;
+        xor ^= nums[i];
+    }
+
+    return xor;
+};
