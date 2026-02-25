8d20f21bbad009f5622faa8cb90dcffad4a87b49
Naval Kishore <usnavalg@gmail.com>
2026-02-25 18:37:58 +0530

    Time: 0 ms (100%), Space: 8.9 MB (39.82%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <sstream>
+using namespace std;
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string> words;
+        string word;
+        stringstream ss(s);
+
+        while (ss >> word) {
+            words.push_back(word);
+        }
+
+        if (pattern.size() != words.size())
+            return false;
+
+        unordered_map<char, string> charToWord;
+        unordered_map<string, char> wordToChar;
+
+        for (int i = 0; i < pattern.size(); ++i) {
+            char c = pattern[i];
+            const string& w = words[i];
+
+            if (charToWord.count(c)) {
+                if (charToWord[c] != w)
+                    return false;
+            } else {
+                charToWord[c] = w;
+            }
+
+            if (wordToChar.count(w)) {
+                if (wordToChar[w] != c)
+                    return false;
+            } else {
+                wordToChar[w] = c;
+            }
+        }
+
+        return true;
+    }
+};
