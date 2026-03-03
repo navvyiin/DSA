8d04d883fc81605f7672ebf18698f925543a42de
Naval Kishore <usnavalg@gmail.com>
2026-03-03 16:26:43 +0530

    Time: 15 ms (76.63%), Space: 19.8 MB (8.03%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        magazine_count = Counter(magazine)
+        
+        for char in ransomNote:
+            if magazine_count[char] == 0:
+                return False
+            magazine_count[char] -= 1
+        
+        return True
