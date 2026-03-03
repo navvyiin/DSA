8a3a5e2e6b5ca77df7a622b3bfca6fc7c50b54fb
Naval Kishore <usnavalg@gmail.com>
2026-03-03 16:31:05 +0530

    Time: 0 ms (100%), Space: 9.1 MB (94.73%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,19 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char result = 0;
+        
+        for (char c : s) {
+            result ^= c;
+        }
+        
+        for (char c : t) {
+            result ^= c;
+        }
+        
+        return result;
+    }
+};
