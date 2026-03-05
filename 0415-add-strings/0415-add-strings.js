2602f77e2564d95d94f32a51c81f57b749017792
Naval Kishore <usnavalg@gmail.com>
2026-03-05 20:11:49 +0530

    Time: 1 ms (84.89%), Space: 55.7 MB (66.96%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.js
@@ -0,0 +1,20 @@
+var addStrings = function(num1, num2) {
+    let i = num1.length - 1;
+    let j = num2.length - 1;
+    let carry = 0;
+    let result = "";
+
+    while (i >= 0 || j >= 0 || carry > 0) {
+        let digit1 = i >= 0 ? num1.charCodeAt(i) - 48 : 0;
+        let digit2 = j >= 0 ? num2.charCodeAt(j) - 48 : 0;
+
+        let sum = digit1 + digit2 + carry;
+        result = (sum % 10) + result;
+        carry = Math.floor(sum / 10);
+
+        i--;
+        j--;
+    }
+
+    return result;
+};
