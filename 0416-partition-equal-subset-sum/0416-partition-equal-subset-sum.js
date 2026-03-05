b3ce66268a1a4b4263b1c1ae1f66489247510ee3
Naval Kishore <usnavalg@gmail.com>
2026-03-05 20:12:23 +0530

    Time: 46 ms (71.45%), Space: 58.2 MB (72.94%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
@@ -0,0 +1,17 @@
+var canPartition = function(nums) {
+    let total = nums.reduce((a, b) => a + b, 0);
+    
+    if (total % 2 !== 0) return false;
+
+    let target = total / 2;
+    let dp = new Array(target + 1).fill(false);
+    dp[0] = true;
+
+    for (let num of nums) {
+        for (let j = target; j >= num; j--) {
+            dp[j] = dp[j] || dp[j - num];
+        }
+    }
+
+    return dp[target];
+};
