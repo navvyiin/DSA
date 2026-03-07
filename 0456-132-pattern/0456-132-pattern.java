c1026c7f67856b2f17810594bc7150193b77ad6a
Naval Kishore <usnavalg@gmail.com>
2026-03-07 19:25:59 +0530

    Time: 38 ms (73.89%), Space: 98.9 MB (46.21%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> stack = new Stack<>();
+        int third = Integer.MIN_VALUE;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] < third) return true;
+
+            while (!stack.isEmpty() && nums[i] > stack.peek()) {
+                third = stack.pop();
+            }
+
+            stack.push(nums[i]);
+        }
+
+        return false;
+    }
+}
