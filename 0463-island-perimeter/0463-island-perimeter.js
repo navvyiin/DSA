fa6902b1129f0d4008993c6061b96ccb713f96be
Naval Kishore <usnavalg@gmail.com>
2026-03-08 13:34:03 +0530

    Time: 6 ms (68.56%), Space: 63.9 MB (97.6%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.js
@@ -0,0 +1,18 @@
+var islandPerimeter = function(grid) {
+    let perimeter = 0;
+    const rows = grid.length;
+    const cols = grid[0].length;
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (grid[i][j] === 1) {
+                perimeter += 4;
+
+                if (i > 0 && grid[i - 1][j] === 1) perimeter -= 2;
+                if (j > 0 && grid[i][j - 1] === 1) perimeter -= 2;
+            }
+        }
+    }
+
+    return perimeter;
+};
