91204985c5aaaf25a5e7d31a89a9c7097d9ea17e
Naval Kishore <usnavalg@gmail.com>
2026-03-08 20:06:56 +0530

    Time: 1 ms (61.63%), Space: 64.3 MB (94.19%) - LeetHub

---
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+var convertBST = function(root) {
+    let sum = 0;
+
+    function traverse(node) {
+        if (!node) return;
+
+        traverse(node.right);
+        sum += node.val;
+        node.val = sum;
+        traverse(node.left);
+    }
+
+    traverse(root);
+    return root;
+};
