d9148bc1f3bff64cf04343650fd922a79bf14974
Naval Kishore <usnavalg@gmail.com>
2026-03-08 13:27:07 +0530

    Time: 0 ms (100%), Space: 54.1 MB (24.18%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.js
@@ -0,0 +1,11 @@
+var hammingDistance = function(x, y) {
+    let xor = x ^ y;
+    let count = 0;
+
+    while (xor !== 0) {
+        count += xor & 1;
+        xor >>= 1;
+    }
+
+    return count;
+};
