235111d2d725ec8f7f4e2031b3ec2e553009c1e4
Naval Kishore <usnavalg@gmail.com>
2026-03-18 21:58:17 +0530

    Time: 24 ms (77.15%), Space: 49.1 MB (31.43%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> prefixCount = new HashMap<>();
+        prefixCount.put(0, 1);
+
+        int sum = 0, count = 0;
+
+        for (int num : nums) {
+            sum += num;
+
+            if (prefixCount.containsKey(sum - k)) {
+                count += prefixCount.get(sum - k);
+            }
+
+            prefixCount.put(sum, prefixCount.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+}
