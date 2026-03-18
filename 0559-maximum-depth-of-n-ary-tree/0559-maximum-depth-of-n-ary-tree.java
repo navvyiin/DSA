5a6f681f68e896670e33cf0f32c4b3461a28d4cb
Naval Kishore <usnavalg@gmail.com>
2026-03-18 21:57:13 +0530

    Time: 0 ms (100%), Space: 45.4 MB (61.24%) - LeetHub

---
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxDepth(Node root) {
+        if (root == null) return 0;
+        
+        int maxDepth = 0;
+        
+        for (Node child : root.children) {
+            maxDepth = Math.max(maxDepth, maxDepth(child));
+        }
+        
+        return 1 + maxDepth;
+    }
+}
