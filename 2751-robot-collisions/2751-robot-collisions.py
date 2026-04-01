6beb693c8494958823363b83b580d3682b44b912
Naval Kishore <usnavalg@gmail.com>
2026-04-01 18:17:21 +0530

    Time: 86 ms (95.83%), Space: 36.5 MB (89.88%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.py
+++ 2751-robot-collisions/2751-robot-collisions.py
@@ -1,31 +1,28 @@
 class Solution:
     def survivedRobotsHealths(self, positions, healths, directions):
-        n = len(positions)
+        idx = list(range(len(positions)))
+        idx.sort(key=positions.__getitem__)
         
-        robots = sorted([(positions[i], i) for i in range(n)])
+        stack = []
+        health = healths
         
-        stack = []  # store indices of right-moving robots
-        health = healths[:]  # mutable copy
-        
-        for pos, i in robots:
+        for i in idx:
             if directions[i] == 'R':
                 stack.append(i)
             else:
-                while stack and health[i] > 0:
+                while stack and health[i]:
                     j = stack[-1]
                     
                     if health[j] < health[i]:
                         health[i] -= 1
                         health[j] = 0
                         stack.pop()
-                    elif health[j] == health[i]:
+                    elif health[j] > health[i]:
+                        health[j] -= 1
+                        health[i] = 0
+                    else:
                         health[j] = 0
                         health[i] = 0
                         stack.pop()
-                        break
-                    else:
-                        health[j] -= 1
-                        health[i] = 0
-                        break
         
-        return [health[i] for i in range(n) if health[i] > 0]
+        return [h for h in health if h]
