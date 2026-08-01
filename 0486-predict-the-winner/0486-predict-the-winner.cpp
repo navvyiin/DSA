ac0d4d1f348bc0fb5b57a8743c5590ef732f8fb6
Naval Kishore <usnavalg@gmail.com>
2026-08-01 07:14:01 +0530

    Time: 3 ms (39.21%), Space: 10.1 MB (52.91%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,18 +1,14 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        vector<vector<int>> dp(n, vector<int>(n));
 
         for (int i = 0; i < n; i++)
             dp[i][i] = nums[i];
 
         for (int len = 2; len <= n; len++) {
-            for (int i = 0; i <= n - len; i++) {
+            for (int i = 0; i + len - 1 < n; i++) {
                 int j = i + len - 1;
                 dp[i][j] = max(nums[i] - dp[i + 1][j],
                                nums[j] - dp[i][j - 1]);
